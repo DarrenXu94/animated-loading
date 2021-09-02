3d2cb7799c07176c3ab32aa3e66ee701103cdafc
Darren Xu <darrenxu1994@yahoo.com.au>
2021-09-02 17:32:04 +1000

    Side menu

--- src/main/Banner.tsx
+++ src/main/Banner.tsx
@@ -1,6 +1,28 @@
 import React, { useEffect } from "react";
+import { motion } from "framer-motion";
 
 export interface BannerProps {}
+
+const container = {
+  show: {
+    transition: {
+      delayChildren: 1.6,
+      staggerChildren: 0.35,
+    },
+  },
+};
+
+const itemMain = {
+  hidden: { opacity: 0, y: 100 },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      ease: [0.6, 0.01, -0.05, 0.95],
+      duration: 1.6,
+    },
+  },
+};
 
 export default function Banner({}: BannerProps) {
   useEffect(() => {
@@ -19,6 +41,19 @@ export default function Banner({}: BannerProps) {
 
   return (
     <div className="banner">
+      <motion.div
+        className="banner-vertical-menu"
+        initial="hidden"
+        animate="show"
+        variants={container}
+      >
+        <motion.div variants={itemMain} className="banner-menu-item">
+          Home
+        </motion.div>
+        <motion.div variants={itemMain} className="banner-menu-item">
+          Projects
+        </motion.div>
+      </motion.div>
       <div className="circle">
         <div className="logo"></div>
         <div className="text">
